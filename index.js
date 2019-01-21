f6feef8280d39bf673d02f07c1db33fa90d24be3
nasshole <thedude7054@icloud.com>
2019-01-20 22:38:41 -0700

    Migrate to Docker

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var request = require('request');
 
+const pingURL = process.env.PING_URL;
+const botToken = process.env.BOT_TOKEN;
+const checkName = process.env.CHECK_NAME;
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
   var status = 'up';
 function initialCheck(req, res) {
-    request({url : 'https://flydeltavirtual.org', time : true, stream : true}, function (error, response, body) {
+    request({url : pingURL, time : true, stream : true}, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-            client.user.setActivity(`Front Page is UP`, { type: 'WATCHING' })
+            client.user.setActivity(`${checkName} is UP`, { type: 'WATCHING' })
             .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
             .catch(console.error);
             status = 'up';
             console.log('initialCheck: Up')
         } else {
-            client.user.setActivity(`Front Page is DOWN`, { type: 'WATCHING' })
+            client.user.setActivity(`${checkName} is DOWN`, { type: 'WATCHING' })
             .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
             .catch(console.error);
             status = 'down';
@@ -25,16 +29,16 @@ function initialCheck(req, res) {
 };
 setTimeout(initialCheck, 5000);
 function checkStatus(req, res) {
-    request({url : 'https://flydeltavirtual.org', time : true, stream : true}, function (error, response, body) {
+    request({url : pingURL, time : true, stream : true}, function (error, response, body) {
     if (!error && response.statusCode == 200) {
             if (status === 'up') return;
-            client.user.setActivity(`Front Page is UP`, { type: 'WATCHING' })
+            client.user.setActivity(`${checkName} is UP`, { type: 'WATCHING' })
             .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
             .catch(console.error);
             status = 'up';
         } else {
             if (status === 'down') return;
-            client.user.setActivity(`Front Page is DOWN`, { type: 'WATCHING' })
+            client.user.setActivity(`${checkName} is DOWN`, { type: 'WATCHING' })
             .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
             .catch(console.error);
             status = 'down';
@@ -47,4 +51,4 @@ setInterval(()=> {
     console.log('ticked');
 }, 5000);
 
-client.login('NTI4ODU2NjAwMDM1OTgzMzYw.DyXQ_Q.G16Tl0QILldb5045w9IymJYAsy4');
+client.login(botToken);
