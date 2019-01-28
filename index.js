ad4a8f45280cac34d42a00a1abb6d06c9645096d
nasshole <thedude7054@icloud.com>
2019-01-27 20:27:01 -0700

    remove initial check, more customization
    Removed initial check in index.js, and integrated into one check function. Added DM functionality to alert specified users when an issue arises.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var request = require('request');
+var dotenv = require('dotenv').load();
+var status = '';
 
 const pingURL = process.env.PING_URL;
 const botToken = process.env.BOT_TOKEN;
@@ -8,47 +10,46 @@ const checkName = process.env.CHECK_NAME;
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    setInterval(()=> {
+        checkStatus()
+    }, 5000);
 });
-  var status = 'up';
-function initialCheck(req, res) {
-    request({url : pingURL, time : true, stream : true}, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-            client.user.setActivity(`${checkName} is UP`, { type: 'WATCHING' })
-            .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
-            .catch(console.error);
-            status = 'up';
-            console.log('initialCheck: Up')
-        } else {
-            client.user.setActivity(`${checkName} is DOWN`, { type: 'WATCHING' })
-            .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
-            .catch(console.error);
-            status = 'down';
-            console.log('initialCheck: Down')
-        }
-    })
-};
-setTimeout(initialCheck, 5000);
+
+
+
 function checkStatus(req, res) {
     request({url : pingURL, time : true, stream : true}, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-            if (status === 'up') return;
+            console.log("checking UP");
+            if (status === 'UP') return;
+            alert({ service : checkName, status : 'UP', statusCode : response.statusCode })
             client.user.setActivity(`${checkName} is UP`, { type: 'WATCHING' })
             .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
             .catch(console.error);
-            status = 'up';
+            status = 'UP';
         } else {
-            if (status === 'down') return;
+            console.log("checking DOWN");
+            if (status === 'DOWN') return;
+            alert({ service : checkName, status : 'DOWN', statusCode : response.statusCode })
             client.user.setActivity(`${checkName} is DOWN`, { type: 'WATCHING' })
             .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
             .catch(console.error);
-            status = 'down';
+            status = 'DOWN';
         }
     })
 };
 
-setInterval(()=> {
-    checkStatus()
-    console.log('ticked');
-}, 5000);
+async function alert(options = {}) {
+    const {checkName , status, statusCode} = options;
+    var dmUsers = process.env.DM_USERS.split(' ');
+    console.log(`Users Array: ${dmUsers}`);
+    dmUsers.forEach(function(id){
+        client.users.get(id).createDM();           // Using process.env.CHECK_NAME because checkName returned undefined this works just as well
+        client.users.get(id).send(`**Service:** ${process.env.CHECK_NAME}\n**Status:** ${status}\n**Status Code:** ${statusCode}`);
+    });
+}
+
+
+
 
 client.login(botToken);
