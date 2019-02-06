81ebbdb50ff3a7eaeda4a935d31394b54ff145f4
nasshole <thedude7054@icloud.com>
2019-02-05 17:16:57 -0700

    Add development GCR push

--- deploy-development.sh
+++ deploy-development.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build -t gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME_PRODUCTION}:$TRAVIS_COMMIT .
+docker build -t gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME_DEVELOPMENT}:$TRAVIS_COMMIT .
 
 echo $GCLOUD_SERVICE_KEY_PRD | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
@@ -12,14 +12,14 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME_PRD
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME_PRD
 
-gcloud docker push gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME_PRODUCTION}
+gcloud docker push gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME_DEVELOPMENT}
 
-yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME_PRD}/$DOCKER_IMAGE_NAME_PRODUCTION}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME_PRD}/$DOCKER_IMAGE_NAME_PRODUCTION}:latest
+yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME_PRD}/$DOCKER_IMAGE_NAME_DEVELOPMENT}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME_DEVELOPMENT}:latest
 
 kubectl config view
 kubectl config current-context
 
-kubectl set image deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_CONTAINER_NAME}=gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME_PRODUCTION}:$TRAVIS_COMMIT
+kubectl set image deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_CONTAINER_NAME}=gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME_DEVELOPMENT}:$TRAVIS_COMMIT
 
 # sleep 30
 # npm run e2e_test
